456800442d750a55482bc963de9bf5127d15a5c7
soundaryasabapathy <33174855+soundaryasabapathy@users.noreply.github.com>
2017-11-13 12:33:35 +0530

    Create alpha.py

---
+++ alpha.py
@@ -0,0 +1,6 @@
+a=["a","b","C","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+b="r"
+if(b in a):
+    print('alphabet')
+else:
+    print('not alphabet')
